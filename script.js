ba19d6d0437c9cfc133f507b53bc42ee6ab44f24
PRRInc <prrincdev@gmail.com>
2024-04-21 17:14:49 -0400

    Pokemon in-class Exercise complete

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-.then((response) => response.json())
-.then(console.log)
-.catch(console.log)
+
+function getPokemonByID(id) {
+    fetch(BASE_URL + "/" + id)
+    .then((res) => res.json())
+    .then((res) => {
+        const article = document.createElement("article")
+        console.log(res);
+        article.innerHTML = `<img src=${res.sprites.front_default} alt=${res.name} />
+        <h2>${res.name} (#${res.order})</h2>`
+
+        document.querySelector(".pokemon").append(article);
+    })
+    .catch((error) => {
+        console.log("error triggered", error);
+        const errorSection = document.createElement("section");
+        errorSection.setAttribute("class", "error");
+
+        errorSection.innerHTML = `<p>There was an error!</p>
+        <p class="message">${error}</p>`
+
+        document.querySelector("main").append(errorSection);
+    });
+
+
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // const input = document.getElementById("pokemon-id")
+    console.log(e.target.id.value);
+    getPokemonByID(e.target.id.value);
+    e.target.id.value = "";
+});
+
+
+
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+// .then((response) => response.json())
+// .then(console.log)
+// .catch(console.log)
