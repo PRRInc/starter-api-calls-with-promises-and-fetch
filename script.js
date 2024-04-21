3cc6ebb42c4c2cdd4978d645e7bbcb481379ab6a
PRRInc <prrincdev@gmail.com>
2024-04-21 15:10:31 -0400

    Update with fetch

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+
+fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+.then((response) => response.json())
+.then(console.log)
+.catch(console.log)
